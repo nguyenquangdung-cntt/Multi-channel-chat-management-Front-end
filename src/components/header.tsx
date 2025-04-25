4355af5d8a6c2419843ff47d0176ec87218433d8
Dũng Nguyễn <nguyenquangdung2309@gmail.com>
2025-04-26 06:29:52 +0700

    fix api

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -68,11 +68,11 @@ export default function Header() {
       setUser(JSON.parse(savedUser));
     }
 
-    // const savedPage = localStorage.getItem("fb_page");
-    // if (savedPage) {
-    //   const { pageID, pageAccessToken } = JSON.parse(savedPage);
-    //   // Lưu pageID và pageAccessToken vào state hoặc thực hiện các hành động khác với chúng
-    // }
+    const savedPage = localStorage.getItem("fb_page");
+    if (savedPage) {
+      const { pageID, pageAccessToken } = JSON.parse(savedPage);
+      // Lưu pageID và pageAccessToken vào state hoặc thực hiện các hành động khác với chúng
+    }
   }, []);
 
   const handleLogin = () => {
@@ -124,7 +124,8 @@ export default function Header() {
   ) => {
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/api/facebook-auth", {
+      const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+      const res = await fetch(`${API_URL}/api/facebook-auth`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
