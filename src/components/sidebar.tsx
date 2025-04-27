77b69ab72ceb959b8ac6115389d74110f43b6af0
Dũng Nguyễn <nguyenquangdung2309@gmail.com>
2025-04-27 07:21:07 +0700

    fix sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -6,7 +6,7 @@ import { usePathname } from "next/navigation";
 interface SidebarProps {
     isOpen: boolean;
     toggle: () => void;
-  }
+}
   
 export default function Sidebar({ isOpen, toggle }: SidebarProps) {
     const pathname = usePathname();
@@ -19,27 +19,25 @@ export default function Sidebar({ isOpen, toggle }: SidebarProps) {
         >
             <button
                 onClick={toggle}
-                className="mb-4 bg-gray-600 px-3 py-1 rounded hover:bg-gray-500 transition"
-            >
+                className="mb-4 bg-gray-600 p-3 rounded hover:bg-gray-500 transition flex justify-center"
+                >
                 <FontAwesomeIcon icon={faBars} />
             </button>
-            {isOpen && (
-                <ul className="space-y-2">
-                    <li className={`hover:text-blue-400 cursor-pointer p-2 rounded ${pathname === "/" ? "text-blue-500 font-bold bg-white" : ""}`}>
-                        <Link href="/">
-                            <FontAwesomeIcon icon={faHouse} className="w-4 h-4 mr-[5px]" />
-                            Dashboard
-                        </Link>
-                    </li>
-                    <li className={`hover:text-blue-400 cursor-pointer p-2 rounded ${pathname === "/messenger" ? "text-blue-500 font-bold bg-white" : ""}`}>
-                        <Link href="/messenger">
-                            <FontAwesomeIcon icon={faCommentDots} className="w-4 h-4 mr-[5px]" />
-                            Messenger
-                        </Link>
-                    </li>
-                </ul>
-            )}
+
+            <ul className="space-y-2">
+                <li className={`hover:text-blue-400 cursor-pointer p-2 rounded flex items-center gap-2 ${pathname === "/" ? "text-blue-500 font-bold bg-white" : ""}`}>
+                    <Link href="/" className="flex items-center">
+                        <FontAwesomeIcon icon={faHouse} className="w-5 h-5" />
+                        {isOpen && <span className="ml-2">Dashboard</span>}
+                    </Link>
+                </li>
+                <li className={`hover:text-blue-400 cursor-pointer p-2 rounded flex items-center gap-2 ${pathname === "/messenger" ? "text-blue-500 font-bold bg-white" : ""}`}>
+                    <Link href="/messenger" className="flex items-center">
+                        <FontAwesomeIcon icon={faCommentDots} className="w-5 h-5" />
+                        {isOpen && <span className="ml-2">Messenger</span>}
+                    </Link>
+                </li>
+            </ul>
         </div>
     );
 }
-  
